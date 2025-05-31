e2f87fb7535de44967296d5f5719ce09852613b2
Meghna Reddy <meghnareddy2403@gmail.com>
2025-05-31 20:26:18 +0530

    if and else

--- module_2/if and else/demo.py
+++ module_2/if and else/demo.py
@@ -4,7 +4,7 @@ menu = {
     'poori':20,
     'upma':30
 }
-item_1 = input('enter the item:')
+item_1 = input('Enter the item:')
 item_1_count = int(input('enter the count:'))
 
 item_1_price = menu[item_1]
